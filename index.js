21be8d6862998e281a396ee21a7e62c387876078
apocalypse575 <apocalypse575@gmail.com>
2017-09-28 16:45:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push('Ralph')
+  return name
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift('Bob')
+  return name
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop('name')
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift('name')
+  return kittens
+}
+
+function appendKitten(name) {
+  var newKittens = [...kittens, 'Broom']
+  return newKittens
+}
+
+function prependKitten(name) {
+  var newKittens = ['Arnold', ...kittens]
+  return newKittens
+}
